f2b7dcbe1d5e02765393143da782a4faf8e36469
jbeltrami <joao@primarydesign.com>
2018-09-21 14:23:09 -0400

    Finish lesson 5.
    Add GreenSock to project, create a parallax animation effect.

--- 05-simple-parallax/start/js/main.js
+++ 05-simple-parallax/start/js/main.js
@@ -1,60 +1,58 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  // Init ScrollMagic
+  const controller = new ScrollMagic.Controller()
 
-	// Init ScrollMagic
-	var controller = new ScrollMagic.Controller();
+  // pin the intro
+  const pinIntroScene = new ScrollMagic.Scene({
+    triggerElement: '#intro',
+    triggerHook: 0,
+    duration: '30%'
+  })
+    .setPin('#intro')
+    .addTo(controller)
 
-	// pin the intro
-	var pinIntroScene = new ScrollMagic.Scene({
-		triggerElement: '#intro',
-		triggerHook: 0,
-		duration: '30%'
-	})
-	.setPin('#intro', {pushFollowers: false})
-	.addTo(controller);
+  // parallax scene
+  var parallaxTl = new TimelineMax();
+  parallaxTl
+    .from('.content-wrapper', 0.4, {autoAlpha: 0, ease: Power0.easeNone}, 0.4)
+    .from('.bcg', 2, {y: '-50%', ease: Power0.easeNone}, 0)
+    ;
 
-	// pin again
-	var pinIntroScene2 = new ScrollMagic.Scene({
-		triggerElement: '#project01',
-		triggerHook: 0.4
-	})
-	.setPin('#intro', {pushFollowers: false})
-	.addTo(controller);
+  const slideParallaxScene = new ScrollMagic.Scene({
+    triggerElement: '.bcg-parallax',
+    triggerHook: 1,
+    duration: '100%'
+  })
+    .setTween(parallaxTl)
+    .addTo(controller)
 
-	// loop through each .project element
-	$('.project').each(function(){
+  // const showParallaxText = new ScrollMagic.Scene({
+  //   triggerElement: '.content-wrapper',
+  //   triggerHook: 0.75
+  // })
+  //   .setClassToggle('.content-wrapper', 'fade-in')
+  //   .addIndicators({
+  //     name: 'show text',
+  //     colorTrigger: 'blue',
+  //     colorStart: 'red',
+  //     colorEnd: 'green'
+  //   })
+  //   .addTo(controller)
 
-		// build a scene
-		var ourScene = new ScrollMagic.Scene({
-			triggerElement: this.children[0],
-			triggerHook: 0.9
-		})
-		.setClassToggle(this, 'fade-in') // add class to project01
-		.addIndicators({
-			name: 'fade scene',
-			colorTrigger: 'black',
-			colorStart: '#75C695',
-			colorEnd: 'pink'
-		}) // this requires a plugin
-		.addTo(controller);
-
-	});
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // loop through each .project element
+  $('.project').each(function () {
+    // build a scene
+    const ourScene = new ScrollMagic.Scene({
+      triggerElement: this.children[0],
+      triggerHook: 0.9
+    })
+      .setClassToggle(this, 'fade-in') // add class to project01
+      .addIndicators({
+        name: 'fade scene',
+        colorTrigger: 'black',
+        colorStart: '#75C695',
+        colorEnd: 'pink'
+      }) // this requires a plugin
+      .addTo(controller)
+  })
+})
