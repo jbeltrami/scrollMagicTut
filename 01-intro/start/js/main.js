cb4688ebdb5fcaef02dcec564d30d903cf26d9dd
jbeltrami <joao@primarydesign.com>
2018-09-21 12:58:32 -0400

    Finish of lesson 3.
    Add loop and reverse properties to create new scenes and append to 
    controller dynamically.

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -1,19 +1,23 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // Init img_scrollmagic
   const controller = new ScrollMagic.Controller()
 
-  // build a Scene
-  const ourScene = new ScrollMagic.Scene({
-    triggerElement: '#project01 img',
-    duration: '100%', // this value is based on the viewport height
-    triggerHook: 0.9 // this is how close to the bottom you want the trigger to be. 0 means at the top of viewport and 1 is the bottom
-  })
-    .setClassToggle('#project01', 'fade-in')
-    .addIndicators({ // This requires a plugin called addIndicators, that can be found in scrollMagic's docs
-      name: 'fade scene',
-      colorTrigger: 'black',
-      colorStart: '#75C695',
-      colorEnd: 'pink'
+  // loop to each .project element
+  $('.project').each(function () {
+
+    console.log(this)
+    // build a Scene
+    const ourScene = new ScrollMagic.Scene({
+      triggerElement: this.children[0],
+      triggerHook: 0.75, // this is how close to the bottom you want the trigger to be. 0 means at the top of viewport and 1 is the bottom
+      reverse: false
     })
-    .addTo(controller)
+      .setClassToggle(this, 'fade-in')
+      .addIndicators({
+        name: 'fade scene',
+        colorTrigger: 'black',
+        colorStart: '#75C695',
+        colorEnd: 'pink'})// This requires a plugin called addIndicators, that can be found in scrollMagic's docs
+      .addTo(controller)
+  })
 })
