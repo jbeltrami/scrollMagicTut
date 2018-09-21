95c0484d14afb43c2d1f4be9ff93661d5b599d85
jbeltrami <joao@primarydesign.com>
2018-09-21 13:27:01 -0400

    Finish lesson 4.
    Pin an element to the screen, then have the same element not 'hide' 
    behind next section.

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -1,23 +1,42 @@
-$(document).ready(function() {
-  // Init img_scrollmagic
-  const controller = new ScrollMagic.Controller()
+$(document)
+  .ready(function () {
+    // Init img_scrollmagic
+    const controller = new ScrollMagic.Controller()
 
-  // loop to each .project element
-  $('.project').each(function () {
+    const pinIntroScene = new ScrollMagic.Scene({
+        triggerElement: '#intro',
+        triggerHook: 0,
+        duration: '30%'
+      })
+      .setPin('#intro', {
+        pushFollowers: false
+      })
+      .addTo(controller);
 
-    console.log(this)
-    // build a Scene
-    const ourScene = new ScrollMagic.Scene({
-      triggerElement: this.children[0],
-      triggerHook: 0.75, // this is how close to the bottom you want the trigger to be. 0 means at the top of viewport and 1 is the bottom
-      reverse: false
-    })
-      .setClassToggle(this, 'fade-in')
-      .addIndicators({
-        name: 'fade scene',
-        colorTrigger: 'black',
-        colorStart: '#75C695',
-        colorEnd: 'pink'})// This requires a plugin called addIndicators, that can be found in scrollMagic's docs
-      .addTo(controller)
+    const pinIntroSceneEnd = new ScrollMagic.Scene({
+        triggerElement: '#project01',
+        triggerHook: 0.4
+      })
+      .setPin('#intro', {
+        pushFollowers: false
+      })
+      .addTo(controller);
+
+    // loop to each .project element
+    $('.project')
+      .each(function () {
+        // build a Scene
+        const ourScene = new ScrollMagic.Scene({
+            triggerElement: this.children[0],
+            triggerHook: 0.75 // this is how close to the bottom you want the trigger to be. 0 means at the top of viewport and 1 is the bottom
+          })
+          .setClassToggle(this, 'fade-in')
+          .addIndicators({
+            name: 'fade scene',
+            colorTrigger: 'black',
+            colorStart: '#75C695',
+            colorEnd: 'pink'
+          }) // This requires a plugin called addIndicators, that can be found in scrollMagic's docs
+          .addTo(controller)
+      })
   })
-})
